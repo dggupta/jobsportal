652c1dd559abb0538e2c5c55196b6d9b9c824136
dggupta <divyanshgupta96302@gmail.com>
2025-04-02 16:50:49 +0530

    finalchange

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin : "https://your-frontend-url.onrender.com" || 'http://localhost:5173',
+    origin : "https://jobsportal-2-z8uk.onrender.com" || 'http://localhost:5173',
     credentials:true
 }
 
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://your-backend-url.onrender.com/api/v1/user" || "http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="https://your-backend-url.onrender.com/api/v1/job"||"http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="https://your-backend-url.onrender.com/api/v1/application" || "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT= "https://your-backend-url.onrender.com/api/v1/company"|| "http://localhost:8000/api/v1/company";
+export const USER_API_END_POINT="https://jobsportal-2-z8uk.onrender.com/api/v1/user" || "http://localhost:8000/api/v1/user";
+export const JOB_API_END_POINT="https://jobsportal-2-z8uk.onrender.com/api/v1/job"||"http://localhost:8000/api/v1/job";
+export const APPLICATION_API_END_POINT="https://jobsportal-2-z8uk.onrender.com/api/v1/application" || "http://localhost:8000/api/v1/application";
+export const COMPANY_API_END_POINT= "https://jobsportal-2-z8uk.onrender.com/api/v1/company"|| "http://localhost:8000/api/v1/company";
