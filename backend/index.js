854991abf822201fddf4ba6fc681bb48ad44aa80
dggupta <divyanshgupta96302@gmail.com>
2025-04-02 16:42:04 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -18,13 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin : "https://your-frontend-url.onrender.com" || 'http://localhost:5173',
     credentials:true
 }
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
+const PORT =  process.env.PORT || 3000;
 
 
 // api's
